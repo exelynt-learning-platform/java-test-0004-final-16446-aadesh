9b5095688ec0b94ff6bea88770fde07be3d59e90
Aadesh Minz <aadeshminz31@gmail.com>
2026-03-14 14:04:05 +0530

    Added solution for Question 4

---
+++ JAVA_TEST_0004.java
@@ -0,0 +1,20 @@
+public class JAVA_TEST_0004 {
+    public static void main(String[] args) {
+        int n = 4;
+        int size = 2 * n - 1;
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                int minDistance = Math.min(Math.min(i - 1, size - i), Math.min(j - 1, size - j));
+                
+                System.out.print(n - minDistance);
+                
+                if (j < size) {
+                    System.out.print(" ");
+                }
+            }
+            
+            System.out.println();
+        }
+    }
+}
